65714b8f36220122103674a4bba8a72464f8e5f4
Hee Kang <kanglm32@gmail.com>
2018-05-07 13:42:14 -0700

    update 1

--- templates/cgi-bin/test.py
+++ templates/cgi-bin/test.py
@@ -1,3 +1,4 @@
+import cgi
 from flask import Flask, render_template
 import pandas as pd
 import numpy as np
@@ -19,47 +20,16 @@ from keras.layers.embeddings import Embedding
 from keras.models import load_model
 import warnings
 
+form = cgi.FieldStorage()
+searchterm =  form.getvalue('searchbox')
 
-app = Flask(__name__)
-
-
-def get_tokenizer():
-    tokenizer_file_name = os.path.join('Data', 'tokenizers', 'tokenizer_' + str(200) + '.pkl')
-    time_start = datetime.now()
-    if os.path.isfile(tokenizer_file_name):
-        print('Loading tokenizer...')
-        with open(tokenizer_file_name, 'rb') as file:
-            tokenizer = pickle.load(file)
-    else:
-        print('Training tokenizer...')
-        tokenizer = Tokenizer(num_words=200)
-        # tokenizer.fit_on_texts(train_text)
-        
-        with open(tokenizer_file_name, 'wb') as file:
-            pickle.dump(tokenizer, file)
-        
-    print('Got tokenizer for vocab size: ' + str(200) + ' in ' + str(datetime.now() - time_start))
-    return tokenizer
-
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-
-@app.route("/GuessGame.html")
-def test():
-
-    return render_template('GuessGame.html')
-
-@app.route("/hee/<sentence>")
 def word_to_predict(sentence):
-# def word_to_predict():
     model = load_model(os.path.join('Data', 'models', 'yelp_trained.hd5'))
     list_review = []
-    tokenizer = get_tokenizer()
+    tokenizer = get_tokenizer(sentence)
     tokenizer.fit_on_texts(list_review)
-    # list_review.append(sentence)
-    list_review.append("hello javascript is very hard and i am tired of doing it, but i will preservere.")
+    list_review.append(sentence)
+    # list_review.append("hello javascript is very hard and i am tired of doing it, but i will preservere.")
     list_review = tokenizer.texts_to_sequences(list_review)
     list_review = pad_sequences(list_review, maxlen=250)
     x = model.predict(list_review)
@@ -76,5 +46,4 @@ def word_to_predict(sentence):
     + str(dict['5']) + "%" + " confident of rating being 5. \n"
          "Therefore, I predict the rating to be " + str(score))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+word_to_predict(searchterm)
